8f320d7ab0c171df6aaa024298d7ac24c084ee51
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2022-08-17 11:28:28 +0700

    added handling refresh token fail

--- app/services/mongoose/refreshToken.js
+++ app/services/mongoose/refreshToken.js
@@ -2,6 +2,7 @@ const UserRefreshToken = require("../../api/v1/userRefreshToken/model");
 const User = require("../../api/v1/users/model");
 const jwt = require("jsonwebtoken");
 const { jwtRefreshSecret } = require("../../config");
+const { Unauthenticated } = require("../../errors");
 
 const createUserRefreshToken = async (payload) => {
     const result = await UserRefreshToken.create(payload);
@@ -13,9 +14,23 @@ const getUserRefreshToken = async (req) => {
     const { refreshToken } = req.params;
     const result = await UserRefreshToken.findOne({ refreshToken });
 
-    const payload = jwt.verify(result.refreshToken, jwtRefreshSecret, {
-        algorithms: "HS512",
-    });
+    let payload;
+    jwt.verify(
+        result.refreshToken,
+        jwtRefreshSecret,
+        {
+            algorithms: "HS512",
+        },
+        (err, decoded) => {
+            if (err)
+                throw new Unauthenticated(
+                    err.message ||
+                        "Unauthorized, please login with a valid credential"
+                );
+
+            payload = decoded;
+        }
+    );
 
     const userCheck = await User.findOne({ email: payload.email });
 
