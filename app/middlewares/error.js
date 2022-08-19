7de75d1c3d70118b35a3232284fc7b7d4c0a50ce
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2022-08-19 10:47:40 +0700

    fix: error handling middleware

--- app/middlewares/error.js
+++ app/middlewares/error.js
@@ -7,19 +7,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
     if (err.name === "ValidationError") {
         customError.msg = Object.values(err.errors).map((item) => item.message);
-        customError.statusCode = 400;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
     if (err.code && err.code === 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(
             err.keyValue
         )} field, please choose another value`;
-        customError.statusCode = 400;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
     if (err.name === "CastError") {
         customError.msg = err.message;
-        customError.statusCode = 500;
+        customError.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
     }
 
     return res.status(customError.statusCode).json({ msg: customError.msg });
